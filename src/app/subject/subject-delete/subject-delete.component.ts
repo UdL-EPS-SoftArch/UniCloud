0c8e7c83bd168aa78a237b9271dbd0b38613c429
Buisi97 <oscar.buisan@gmail.com>
2022-05-13 11:10:25 +0200

    Delete subject

--- src/app/subject/subject-delete/subject-delete.component.ts
+++ src/app/subject/subject-delete/subject-delete.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Subject} from '../subject';
+import {ActivatedRoute, Router} from '@angular/router';
+import {SubjectService} from '../subject.service';
+import {AuthenticationBasicService} from '../../login-basic/authentication-basic.service';
 
 @Component({
   selector: 'app-subject-delete',
@@ -6,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./subject-delete.component.css']
 })
 export class SubjectDeleteComponent implements OnInit {
+  public subject: Subject = new Subject();
+  private id: string;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private subjectService: SubjectService,
+              private authenticationService: AuthenticationBasicService) { }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.paramMap.get('id');
+    this.subjectService.getResource(this.id).subscribe(
+      subject => this.subject = subject);
+
   }
 
+  delete(): void {
+    this.subjectService.deleteResource(this.subject).subscribe(
+      () => {
+      this.router.navigate(['/subjects']);
+    });
+  }
 }
