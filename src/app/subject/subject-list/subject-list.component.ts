1b49954bece273e419ca7b023922e78ad3e7d068
Buisi97 <oscar.buisan@gmail.com>
2022-05-13 11:13:17 +0200

    List subject

--- src/app/subject/subject-list/subject-list.component.ts
+++ src/app/subject/subject-list/subject-list.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {PagedResourceCollection} from '@lagoshny/ngx-hateoas-client';
+import {Subject} from '../subject';
+import {Router} from '@angular/router';
+import {SubjectService} from '../subject.service';
+import {AuthenticationBasicService} from '../../login-basic/authentication-basic.service';
 
 @Component({
   selector: 'app-subject-list',
@@ -6,10 +11,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./subject-list.component.css']
 })
 export class SubjectListComponent implements OnInit {
+  public subjectsPagedResource: PagedResourceCollection<Subject>;
+  public subjects: Subject[] = [];
+  public pageSize = 2;
+  public page = 1;
+  public totalSubjects = 0;
 
-  constructor() { }
+  constructor(public router: Router,
+              private subjectService: SubjectService,
+              private authenticationService: AuthenticationBasicService) {
+  }
 
   ngOnInit(): void {
+    this.subjectService.getPage({pageParams: {size: this.pageSize}, sort: {name: 'ASC'}}).subscribe(
+      (page: PagedResourceCollection<Subject>) => {
+        this.subjectsPagedResource = page;
+        this.subjects = page.resources;
+        this.totalSubjects = page.totalElements;
+      });
+  }
+  changePage(): void {
+    this.subjectsPagedResource.customPage({pageParams: {page: this.page - 1, size: this.pageSize}, sort: {name: 'ASC'}}).subscribe(
+      (page: PagedResourceCollection<Subject>) => {
+        this.subjects = page.resources;
+      });
+  }
+  modifyList(subjectPagedResource: PagedResourceCollection<Subject>): void {
+    this.subjectsPagedResource = subjectPagedResource;
+    this.subjects = this.subjectsPagedResource.resources;
+    this.totalSubjects = this.subjectsPagedResource.totalElements;
+  }
+  isRole(role: string): boolean {
+    return  this.authenticationService.isRole(role);
   }
 
 }
