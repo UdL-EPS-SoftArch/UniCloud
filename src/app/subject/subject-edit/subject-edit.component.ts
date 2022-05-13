82fe74f0648a50c6a6cded4ff3a915e8120bbb56
Buisi97 <oscar.buisan@gmail.com>
2022-05-13 11:10:48 +0200

    Edit subject

--- src/app/subject/subject-edit/subject-edit.component.ts
+++ src/app/subject/subject-edit/subject-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Subject} from '../subject';
+import {ActivatedRoute, Router} from '@angular/router';
+import {SubjectService} from '../subject.service';
 
 @Component({
   selector: 'app-subject-edit',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./subject-edit.component.css']
 })
 export class SubjectEditComponent implements OnInit {
+  public subject: Subject = new Subject();
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private subjectService: SubjectService) {
+  }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.subjectService.getResource(id).subscribe(
+      (subject: Subject) => this.subject = subject) ;
+  }
+  onSubmit(): void {
+    this.subjectService.patchResource(this.subject).subscribe(
+      (patchedSubject: Subject) => {
+        this.router.navigate(['subjects', patchedSubject.id]);
+      });
+  }
+  getCurrentSubjectID(): string {
+    return this.subject.id;
   }
 
 }
