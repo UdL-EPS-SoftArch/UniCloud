84c50d4ab787d117a38e73f112212e521646ec8d
Buisi97 <oscar.buisan@gmail.com>
2022-05-13 11:13:32 +0200

    Search subject

--- src/app/subject/subject-search/subject-search.component.ts
+++ src/app/subject/subject-search/subject-search.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import {Subject} from '../subject';
+import {SubjectService} from '../subject.service';
+import {Observable} from 'rxjs/internal/Observable';
+import {ResourceCollection} from '@lagoshny/ngx-hateoas-client';
+import { catchError, debounceTime, distinctUntilChanged, map, switchMap, tap} from 'rxjs/operators';
+import {of, OperatorFunction} from 'rxjs';
 
 @Component({
   selector: 'app-subject-search',
   templateUrl: './subject-search.component.html',
   styleUrls: ['./subject-search.component.css']
 })
-export class SubjectSearchComponent implements OnInit {
+export class SubjectSearchComponent {
+  @Output() emitResults: EventEmitter<Subject> = new EventEmitter();
+  searchFailed = false;
+  searching = false;
 
-  constructor() { }
+  constructor(private subjectService: SubjectService) {
+  }
 
-  ngOnInit(): void {
+  autocomplete: OperatorFunction<string, readonly Subject[]> = (text$: Observable<string>) =>
+    text$.pipe(
+      debounceTime(500),
+      distinctUntilChanged(),
+      tap(() => this.searching = true),
+      switchMap(term => term.length < 3 ? of([]) :
+        this.subjectService.findByName(term).pipe(
+          map((collection: ResourceCollection<Subject>) => collection.resources),
+          tap(() => this.searchFailed = false),
+          catchError(() => {
+            this.searchFailed = true;
+            return of([]);
+          })
+        )
+      ),
+      tap(() => this.searching = false )
+    )
+
+  select(item: any): void {
+    this.emitResults.emit(item as Subject);
   }
 
 }
