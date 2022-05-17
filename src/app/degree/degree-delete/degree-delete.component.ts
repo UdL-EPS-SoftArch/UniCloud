73bd3e62f7626ffba50f071ddc05e43dea7d9061
Iñaki <serranoinyaki@gmail.com>
2022-05-17 16:50:22 +0200

    Degree-delete finished

--- src/app/degree/degree-delete/degree-delete.component.ts
+++ src/app/degree/degree-delete/degree-delete.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import { DegreeService } from '../degree.service';
+import {Degree} from '../degree';
 
 @Component({
   selector: 'app-degree-delete',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./degree-delete.component.css']
 })
 export class DegreeDeleteComponent implements OnInit {
+  public degree: Degree = new Degree();
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private degreeService: DegreeService) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.degreeService.getResource(id).subscribe(
+      degree => this.degree = degree);
+  }
+
+  delete(): void {
+    this.degreeService.deleteResource(this.degree).subscribe(
+      () => {
+        this.router.navigate(['/degrees']);
+      });
   }
 
 }
