70c0a44552935ec2fc2c4ed943c045b0ed0ab599
ahmedazzamouri <ahmedazzamouri98@gmail.com>
2022-05-17 16:38:29 +0200

    Admin detail html and ts components modified

--- src/app/admin/admin-detail/admin-detail.component.ts
+++ src/app/admin/admin-detail/admin-detail.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {AdminService} from '../admin.service';
+import {Admin} from '../admin';
+import {AuthenticationBasicService} from '../../login-basic/authentication-basic.service';
+
+
+
+
 
 @Component({
   selector: 'app-admin-detail',
   templateUrl: './admin-detail.component.html',
-  styleUrls: ['./admin-detail.component.css']
 })
 export class AdminDetailComponent implements OnInit {
+  public admin: Admin = new Admin();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute,
+              private adminService: AdminService,
+              private authenticationService: AuthenticationBasicService) {
   }
 
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.adminService.getResource(id).subscribe(
+      admin => {
+        this.admin = admin;
+      });
+  }
+
+  getCurrentUser(): Admin {
+    return this.authenticationService.getCurrentUser();
+  }
 }
