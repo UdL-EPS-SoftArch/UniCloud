28fa5b5c1ba5c49c1eee8a6dc79ad42ee68b5bea
ahmedazzamouri <ahmedazzamouri98@gmail.com>
2022-05-17 17:38:50 +0200

    Admin search html and ts components modified

--- src/app/admin/admin-list/admin-list.component.ts
+++ src/app/admin/admin-list/admin-list.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-admin-list',
   templateUrl: './admin-list.component.html',
-  styleUrls: ['./admin-list.component.css']
 })
 export class AdminListComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  //ngOnInit(): void {
+  //}
 
 }
