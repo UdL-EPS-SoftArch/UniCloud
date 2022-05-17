f4dadd92eb650637bb97af00ba337cd579b5a15d
Iñaki <serranoinyaki@gmail.com>
2022-05-17 16:48:55 +0200

    Degree-detail finished

--- src/app/degree/degree-detail/degree-detail.component.ts
+++ src/app/degree/degree-detail/degree-detail.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {Degree} from '../degree';
+import {DegreeService} from '../degree.service';
+import {AuthenticationBasicService} from '../../login-basic/authentication-basic.service';
+import {University} from '../../university/university';
 
 @Component({
   selector: 'app-degree-detail',
@@ -6,10 +11,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./degree-detail.component.css']
 })
 export class DegreeDetailComponent implements OnInit {
+  public degree: Degree = new Degree();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute,
+              private degreeService: DegreeService,
+              private authenticationService: AuthenticationBasicService) {
   }
 
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.degreeService.getResource(id).subscribe(
+      degree => {
+        degree.getRelation('university').subscribe((university: University) => {
+          degree.university = university;
+        });
+        this.degree = degree;
+      });
+  }
+  isRole(role: string): boolean {
+    return this.authenticationService.isRole(role);
+  }
 }
