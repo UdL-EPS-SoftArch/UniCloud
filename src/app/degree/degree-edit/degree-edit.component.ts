0c0ef9f56b54b15007985c97c0201ccb9c6f9729
xLince <serranoinyaki@gmail.com>
2022-05-18 18:42:23 +0200

    Advanced degree-edit -> To do field university

--- src/app/degree/degree-edit/degree-edit.component.ts
+++ src/app/degree/degree-edit/degree-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {DegreeService} from '../degree.service';
+import {Degree} from '../degree';
 
 @Component({
   selector: 'app-degree-edit',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DegreeEditComponent implements OnInit {
 
-  constructor() { }
+  public degree: Degree = new Degree();
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private degreeService: DegreeService) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.degreeService.getResource(id).subscribe(
+      (degree: Degree) => this.degree = degree);
+  }
+
+  onSubmit(): void {
+    this.degreeService.patchResource(this.degree).subscribe(
+      (patchedDegree: Degree) => {
+        this.router.navigate(['degrees', this.degree.getSelfLinkHref().split('/')[4]]);
+      });
   }
 
 }
