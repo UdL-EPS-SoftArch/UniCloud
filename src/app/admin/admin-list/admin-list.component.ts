088368b56d75ea08cd9b6cfc8c36e5200e75b541
Armand <56553013+armandciutat@users.noreply.github.com>
2022-05-19 15:54:15 +0200

    Fixed compilation errors

--- src/app/admin/admin-list/admin-list.component.ts
+++ src/app/admin/admin-list/admin-list.component.ts
@@ -1,39 +1,38 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-import {AdminService} from '../admin.service';
-import {Router} from '@angular/router';
-import {Admin} from '../admin';
-import { Sort } from '@lagoshny/ngx-hal-client';
+import { AdminService } from '../admin.service';
+import { Admin } from '../admin';
+import { PagedResourceCollection } from '@lagoshny/ngx-hateoas-client';
 
 @Component({
   selector: 'app-admin-list',
-  templateUrl: './admin-list.component.html',
+  templateUrl: './admin-list.component.html'
 })
 export class AdminListComponent implements OnInit {
-  public admin: Admin[] = [];
+  public admins: Admin[] = [];
   public pageSize = 5;
   public page = 1;
-  public totalAdmin = 0;
-  private sorting: Sort[] = [{ path: 'username', order: 'ASC'}];
+  public totalAdmins = 0;
 
   constructor(
-   public router: Router,
-   private adminService: AdminService) {
-   }
+    public router: Router,
+    private userService: AdminService) {
+  }
 
   ngOnInit(): void {
-    this.adminService.getAll({sort: this.sorting}).subscribe(
-      (admin: Admin[]) => {
-        this.admin = admin;
-        this.totalAdmin = this.adminService.totalElement()});
-  });
-}
+    this.userService.getPage({ pageParams:  { size: this.pageSize }, sort: { username: 'ASC' } }).subscribe(
+      (page: PagedResourceCollection<Admin>) => {
+        this.admins = page.resources;
+        this.totalAdmins = page.totalElements;
+      });
+  }
 
   changePage(): void {
-    this.adminService.page(this.page - 1).subscribe(
-      (admin: Admin[]) => this.admin = admin);
+    this.userService.getPage({ pageParams: { page: this.page - 1, size: this.pageSize }, sort: { username: 'ASC' } }).subscribe(
+      (page: PagedResourceCollection<Admin>) => this.admins = page.resources);
   }
 
   detail(admin: Admin): void {
-    this.router.navigate(['admin', admin.id]);
+    this.router.navigate(['admins', admin.id]);
   }
 }
