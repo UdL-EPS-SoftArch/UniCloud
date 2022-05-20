17e963758663700f6c54d60880cc4896069ed532
Buisi97 <oscar.buisan@gmail.com>
2022-05-20 09:17:07 +0200

    e2e

---
+++ e2e/steps/edit-subject.ts
@@ -0,0 +1,26 @@
+import {Given, When} from 'cypress-cucumber-preprocessor/steps';
+import {DataTable} from '@cucumber/cucumber';
+
+Given(/^ I'm in the Subject Detail page$/, () => {
+  cy.visit('http://localhost:4200/subjects/1');
+});
+
+Given(/^ I click on card-text item {string}$/, (value) => {
+  cy.get('.card-text').contains(value).click();
+});
+
+When(/^ The form is filled with subject name {string}, course {number} and optional {string}$/,
+  (subjectName, subjectCourse, subjectOptional) => {
+  cy.get('#name').should('have.value', subjectName);
+  cy.get('#course').should('have.value', subjectCourse);
+  cy.get('#optional').should('have.value', subjectOptional);
+  });
+
+When(/^ I clear and fill the form with$/, (table: DataTable) => {
+  table.rows().forEach((pair: string[]) =>
+  cy.get('#' + pair[0]).clear().type(pair[1]).blur());
+});
+
+When(/^ I clear the {string} field$/, (value) => {
+  cy.get('#' + value).clear();
+});
