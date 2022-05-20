ac49c48ea5dd6937256ce33ab65bf1dd903cf33d
Buisi97 <oscar.buisan@gmail.com>
2022-05-20 10:50:48 +0200

    Subject Feature

--- out/production/UniCloud/delete-universities.ts
+++
@@ -1,9 +0,0 @@
-import {Then, And} from 'cypress-cucumber-preprocessor/steps';
-
-Then('The university with name {string} is not listed', (value) => {
-  cy.get('a').contains(value).should('not.exist');
-});
-
-And('I wait for the {string} content to appear', (value) => {
-  cy.contains(value).should('exist');
-});
