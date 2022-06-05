f7e50f5dac3c3401405ea94f693d0835078e668c
Buisi97 <oscar.buisan@gmail.com>
2022-06-05 19:21:03 +0200

    Subject Feature

---
+++ e2e/features/CreateSubject.feature
@@ -0,0 +1,6 @@
+Feature: Create Subject
+  In order to use the app
+  As a User
+  I want to create subjects
+
+  Scenario: Create a new Subject as an Admin
---
+++ e2e/steps/edit-subject.ts
@@ -0,0 +1,28 @@
+import {Given, When} from 'cypress-cucumber-preprocessor/steps';
+import {DataTable} from '@cucumber/cucumber';
+
+Given('I\'m in the Subject detail page', () => {
+  cy.visit('http://localhost:4200/subjects/1');
+});
+
+Given('I click on card-text item {string}', (value) => {
+  cy.get('.card-text').contains(value).click();
+});
+
+When('The form is filled with subject name {string}, course {number} and optional {string}',
+  (subjectName, subjectCourse, subjectOptional) => {
+  cy.get('#name').should('have.value', subjectName);
+  cy.get('#course').should('have.value', subjectCourse);
+  cy.get('#optional').should('have.value', subjectOptional);
+});
+
+When('I clear and fill the form', (table: DataTable) => {
+  table.rows().forEach((pair: string[]) =>
+    cy.get('#' + pair[0]).clear().type(pair[1]).blur());
+});
+
+When('I clear the {string} field', (value) => {
+  cy.get('#' + value).clear();
+});
+
+
